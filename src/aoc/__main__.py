1d8805ad1210a53b52f09102a26db742a169dbf8
Nicholas Hansen <ndhansen@leskat.net>
2023-12-02 06:55:23 +0000

    Day 2

--- src/aoc/__main__.py
+++ src/aoc/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 from importlib.resources import files
 
-from aoc import day_1, day_2
+from aoc import day_1, day_2, day_3
 from aoc.utils import contents
 
 parser = argparse.ArgumentParser(prog="AOC", description="Advent of Code")
@@ -24,6 +24,9 @@ match args.day:
     case "day_2":
         part_1 = day_2.part_1(puzzle)
         part_2 = day_2.part_2(puzzle)
+    case "day_3":
+        part_1 = day_3.part_1(puzzle)
+        part_2 = day_3.part_2(puzzle)
     case _:
         raise ValueError("Unknown day!")
 
--- src/aoc/day_2.py
+++ src/aoc/day_2.py
@@ -1,11 +1,78 @@
-from typing import Any
+from dataclasses import dataclass
+from typing import Any, NamedTuple
+
+import parse
 
 from aoc.utils.contents import PuzzleInput
 
 
+class Pull(NamedTuple):
+    red: int
+    green: int
+    blue: int
+
+
+@dataclass
+class Game:
+    id: int
+    pulls: list[Pull]
+
+
+def parse_game(line: str) -> Game:
+    game_raw, pulls_raw = line.split(":", maxsplit=1)
+    game_id = int(game_raw.split(" ", maxsplit=1)[1])
+    pulls = []
+    for pull_raw in pulls_raw.split("; "):
+        red_result = parse.search("{:d} red", pull_raw)
+        if red_result is None:
+            red = 0
+        else:
+            red = red_result.fixed[0]
+
+        blue_result = parse.search("{:d} blue", pull_raw)
+        if blue_result is None:
+            blue = 0
+        else:
+            blue = blue_result.fixed[0]
+
+        green_result = parse.search("{:d} green", pull_raw)
+        if green_result is None:
+            green = 0
+        else:
+            green = green_result.fixed[0]
+
+        pulls.append(Pull(red, green, blue))
+    return Game(id=game_id, pulls=pulls)
+
+
+def possible_game(game: Game) -> int:
+    for pull in game.pulls:
+        if pull.red > 12 or pull.green > 13 or pull.blue > 14:
+            return 0
+    return game.id
+
+
 def part_1(puzzle: PuzzleInput) -> Any:
-    return 0
+    games = list(map(parse_game, puzzle.lines))
+    possible = sum(map(possible_game, games))
+    return possible
+
+
+def min_balls(game: Game) -> int:
+    red, green, blue = 0, 0, 0
+    for pull in game.pulls:
+        if pull.red > red:
+            red = pull.red
+        if pull.green > green:
+            green = pull.green
+        if pull.blue > blue:
+            blue = pull.blue
+    return red * green * blue
 
 
 def part_2(puzzle: PuzzleInput) -> Any:
+    games = list(map(parse_game, puzzle.lines))
+    power_set = list(map(min_balls, games))
+    total = sum(power_set)
+    return total
     return 0
