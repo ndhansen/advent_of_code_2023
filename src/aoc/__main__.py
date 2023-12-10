e449e274b239db7652dba94dcab2cddf886a209d
Nicholas Hansen <ndhansen@leskat.net>
2023-12-10 07:42:12 +0000

    Added timing of parts

--- src/aoc/__main__.py
+++ src/aoc/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 from importlib.resources import files
+from timeit import default_timer as timer
 
 from aoc import (
     day_1,
@@ -28,45 +29,62 @@ args = parser.parse_args()
 filename = "test.txt" if args.test else "input.txt"
 path = files("aoc.inputs") / args.day / filename
 puzzle = contents.get_puzzle_input(path)
+start_time = timer()
 
 match args.day:
     case "day_1":
         part_1 = day_1.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_1.part_2(puzzle)
     case "day_2":
         part_1 = day_2.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_2.part_2(puzzle)
     case "day_3":
         part_1 = day_3.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_3.part_2(puzzle)
     case "day_4":
         part_1 = day_4.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_4.part_2(puzzle)
     case "day_5":
         part_1 = day_5.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_5.part_2(puzzle)
     case "day_6":
         part_1 = day_6.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_6.part_2(puzzle)
     case "day_7":
         part_1 = day_7.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_7.part_2(puzzle)
     case "day_8":
         part_1 = day_8.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_8.part_2(puzzle)
     case "day_9":
         part_1 = day_9.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_9.part_2(puzzle)
     case "day_10":
         part_1 = day_10.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_10.part_2(puzzle)
     case "day_11":
         part_1 = day_11.part_1(puzzle)
+        middle_time = timer()
         part_2 = day_11.part_2(puzzle)
     case _:
         raise ValueError("Unknown day!")
 
+end_time = timer()
+
 print("Part 1:")
 print(part_1)
+print("Time taken:", round(middle_time - start_time, 3), "seconds")
+print()
 print("Part 2:")
 print(part_2)
+print("Time taken:", round(end_time - middle_time, 3), "seconds")
