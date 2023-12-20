817b57e12b1a9427bf66afd20a0d420df31119e9
Nicholas Hansen <ndhansen@leskat.net>
2023-12-20 23:51:44 +0100

    Day 20 part 2

--- src/aoc/day_20.py
+++ src/aoc/day_20.py
@@ -1,12 +1,9 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
+import math
 from typing import Any
 from enum import Enum
-from collections import defaultdict
 
 from aoc.utils.contents import PuzzleInput
-
-
-# True is HIGH, False is LOW
 
 
 class ModuleType(Enum):
@@ -75,10 +72,9 @@ def parse_input(lines: list[str]) -> dict[str, Module]:
     return modules_map
 
 
-def push_button(modules: dict[str, Module]) -> tuple[int, int, bool]:
+def push_button(modules: dict[str, Module]) -> tuple[int, int]:
     lows = 1  # 1 for the button push
     highs = 0
-    done = False
     frontier: list[tuple[bool, str, str]] = [(False, "button", "broadcaster")]
 
     while len(frontier) > 0:
@@ -112,19 +108,58 @@ def push_button(modules: dict[str, Module]) -> tuple[int, int, bool]:
                     highs += 1
                     frontier.append((True, module.name, output))
         elif isinstance(module, Output):
-            if signal == False:
-                done = True
+            pass
         else:
             raise ValueError()
 
-    return lows, highs, done
+    return lows, highs
+
+
+def cycle(modules: dict[str, Module], observed: set[str]) -> set[str]:
+    frontier: list[tuple[bool, str, str]] = [(False, "button", "broadcaster")]
+
+    cycle_detected = set()
+
+    while len(frontier) > 0:
+        signal, source, name = frontier.pop(0)
+        module = modules[name]
+
+        if isinstance(module, Broadcast):
+            for output in module.outputs:
+                frontier.append((False, module.name, output))
+        elif isinstance(module, FlipFlop):
+            if signal == False:
+                if module.state == False:
+                    module.state = True
+                    for output in module.outputs:
+                        frontier.append((True, module.name, output))
+                elif module.state == True:
+                    module.state = False
+                    for output in module.outputs:
+                        frontier.append((False, module.name, output))
+        elif isinstance(module, Conjunction):
+            module.memory[source] = signal
+            if all(module.memory.values()):
+                if module.name in observed:
+                    cycle_detected.add(module.name)
+                for output in module.outputs:
+                    frontier.append((False, module.name, output))
+            else:
+                for output in module.outputs:
+                    frontier.append((True, module.name, output))
+        elif isinstance(module, Output):
+            pass
+        else:
+            raise ValueError()
+
+    return cycle_detected
 
 
 def part_1(puzzle: PuzzleInput) -> Any:
     modules = parse_input(puzzle.lines)
     lows, highs = 0, 0
     for _ in range(1000):
-        low, high, _ = push_button(modules)
+        low, high = push_button(modules)
         lows += low
         highs += high
     return lows * highs
@@ -132,35 +167,17 @@ def part_1(puzzle: PuzzleInput) -> Any:
 
 def part_2(puzzle: PuzzleInput) -> Any:
     modules = parse_input(puzzle.lines)
+    observed: dict[str, int | None] = {"rb": None, "gp": None, "ml": None, "bt": None}
+    observed_names = set(observed.keys())
     pushes = 0
+    cycles: list[int] = []
     while True:
         pushes += 1
-        _, __, done = push_button(modules)
-        if done:
+        detected = cycle(modules, observed_names)
+        for name in detected:
+            if not observed[name]:
+                observed[name] = pushes
+        if all(observed.values()):
+            cycles = list(v for v in observed.values() if v)
             break
-    return pushes
-
-# %a -> %b -> %c
-# To get c to send a high: 4 (2^2)
-# TO get c to send a low: 8 (2^3)
-#
-# %a -> %b, &c
-# %b -> &c
-# %a -> %b -> &c
-# %a -> &c
-# 1 low: a on, b off, c[a=high,b=low]
-# 2 low: a off, b on, c[a=low,b=high]
-# 3 low: a on, b on, c[a=high,b=high]
-#
-# %a -> %b, %c, &d
-# %b -> %c, &d
-# %c -> &d
-# a on every 1st, 3rd, 5th cycle etc
-# a off every 2st, 4rd, 6th cycle etc
-# b on every 2nd, 6th, 10th cycle etc
-# b off every 4nd, 8th, 12th cycle etc
-# c on every 4th, 12th, 20th cycle etc
-# c off every 8th, 16th, 24th cycle etc
-# 01010101010101010101
-# 00110011001100110011
-# 00001111000011110000
+    return math.lcm(*cycles)
